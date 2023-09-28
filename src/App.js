a3ed638d8184d69ac61d0cc7cd5bbd2c6708df06
Travis Mix <travismix1980@gmail.com>
2023-09-27 20:59:42 -0700

    App complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import ProfileCard from "./ProfileCard";
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
+import "bulma/css/bulma.css";
 
 function App() {
+  const pdasData = [
+    { title: "Alexa", handle: "@alexa99", image: AlexaImage, desc: "Alexa was created by Amazon and helps you make purchases on Amazon.com" },
+    { title: "Cortana", handle: "@cortana32", image: CortanaImage, desc: "Cortana was created by Microsoft. Who knows what it does?" },
+    { title: "Siri", handle: "@siri01", image: SiriImage, desc: "Siri was created by Apple and is being phased out?" },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <section className="hero is-primary">
+      <div className="hero-body">
+        <p className="title">Personal Digital Assistants</p>
+      </div>
+    </section>
+      <div className="container">
+        <div className="section">
+          <div className="columns">
+            {pdasData.map((data) => (
+              <div className="column is-4">
+                <ProfileCard
+                  title={data.title}
+                  handle={data.handle}
+                  key={data.title}
+                  image={data.image}
+                  desc={data.desc}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
